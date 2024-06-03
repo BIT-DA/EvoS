b9794891888ef994633748970fb4a90dc75f6bc3
mx xie <39591668+Xiemixue@users.noreply.github.com>
2024-06-03 14:20:27 +0800

    Add files via upload

---
+++ networks/fmow.py
@@ -0,0 +1,231 @@
+from copy import deepcopy
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from einops import rearrange
+from torch.autograd import Variable
+from torchvision.models import densenet121, resnet18
+from lightly.models.modules.heads import SimCLRProjectionHead
+from torch.distributions import Normal, Independent
+from .submodules import Attention, DomainDiscriminator, DomainAdversarialLoss
+
+IMG_HEIGHT = 224
+NUM_CLASSES = 62
+
+class FMoWNetwork(nn.Module):
+    def __init__(self, args, weights=None, ssl_training=False):
+        super(FMoWNetwork, self).__init__()
+        self.args = args
+        self.num_classes = NUM_CLASSES
+        
+        if args.dim_bottleneck_f is not None:
+            self.bottleneck = nn.Sequential(
+                nn.Linear(1024, args.dim_bottleneck_f),
+                nn.BatchNorm1d(args.dim_bottleneck_f),
+                nn.ReLU()
+            )
+            self.enc = nn.Sequential(densenet121(pretrained=True).features, nn.ReLU(), nn.AdaptiveAvgPool2d(output_size=(1, 1)), nn.Flatten(), self.bottleneck)
+            self.feature_dim = args.dim_bottleneck_f
+        else:
+            self.enc = nn.Sequential(densenet121(pretrained=True).features, nn.ReLU(), nn.AdaptiveAvgPool2d(output_size=(1, 1)), nn.Flatten())
+            self.feature_dim = 1024
+        
+        self.classifier = nn.Linear(self.feature_dim, self.num_classes)
+        if weights is not None:
+            self.load_state_dict(deepcopy(weights))
+
+        # SimCLR projection head
+        if self.args.method == 'simclr':
+            from lightly.models.modules.heads import SimCLRProjectionHead
+            self.projection_head = SimCLRProjectionHead(self.feature_dim, self.feature_dim, 128)
+
+        # SwaV: projection head and prototypes
+        elif self.args.method == 'swav':
+            from lightly.models.modules import SwaVProjectionHead, SwaVPrototypes
+            self.projection_head = SwaVProjectionHead(self.feature_dim, self.feature_dim, 128)
+            self.prototypes = SwaVPrototypes(128, n_prototypes=self.feature_dim)
+        self.ssl_training = ssl_training
+
+    def forward(self, x):
+        out = self.enc(x)
+
+        if self.args.method == 'simclr' and self.ssl_training:
+            return self.projection_head(out)
+        elif self.args.method == 'swav' and self.ssl_training:
+            out = self.projection_head(out)
+            out = nn.functional.normalize(out, dim=1, p=2)
+            out = self.prototypes(out)
+            return out
+        else:
+            return self.classifier(out)
+
+
+
+class FMoWNetwork_for_EvoS(nn.Module):
+    def __init__(self, args, weights=None):
+        super(FMoWNetwork_for_EvoS, self).__init__()
+        self.args = args
+        self.num_classes = NUM_CLASSES
+
+        if args.dim_bottleneck_f is not None:
+            self.bottleneck = nn.Sequential(
+                nn.Linear(1024, args.dim_bottleneck_f),
+                nn.BatchNorm1d(args.dim_bottleneck_f),
+                nn.ReLU()
+            )
+            self.enc = nn.Sequential(densenet121(pretrained=True).features, nn.ReLU(), nn.AdaptiveAvgPool2d(output_size=(1, 1)), nn.Flatten(), self.bottleneck)
+            self.feature_dim = args.dim_bottleneck_f
+        else:
+            self.enc = nn.Sequential(densenet121(pretrained=True).features, nn.ReLU(), nn.AdaptiveAvgPool2d(output_size=(1, 1)), nn.Flatten())
+            self.feature_dim = 1024
+
+        if weights is not None:
+            self.load_state_dict(deepcopy(weights))
+
+        self.classifier = nn.Linear(self.feature_dim, NUM_CLASSES)
+
+        self.attention_dict = {}
+        if args.dim_head is None:
+            self.attention_dict[1] = Attention(dim=2 * self.feature_dim, heads=args.num_head,
+                                               dim_head=2 * self.feature_dim // args.num_head).cuda()
+        else:
+            self.attention_dict[1] = Attention(dim=2 * self.feature_dim, heads=args.num_head,
+                                               dim_head=args.dim_head).cuda()
+
+        self.conv1d_dict = {}
+        for k in range(2, args.scale + 1):
+            self.conv1d_dict[k] = nn.Unfold(kernel_size=(1, k), stride=1).cuda()  # overlapping window
+            if args.dim_head is None:
+                self.attention_dict[k] = Attention(dim=k * 2 * self.feature_dim, heads=k * args.num_head,
+                                                   dim_head=k * 2 * self.feature_dim // (k * args.num_head)).cuda()
+            else:
+                self.attention_dict[k] = Attention(dim=k * 2 * self.feature_dim, heads=k * args.num_head,
+                                                   dim_head=args.dim_head).cuda()
+
+        self.init_pool = {0: nn.Parameter(torch.zeros([1, 2 * self.feature_dim], requires_grad=True)),
+                          1: nn.Parameter(torch.zeros([1, 2 * self.feature_dim], requires_grad=True))}
+        self.memory_pool = {}
+
+        self.domain_discriminator = DomainDiscriminator(in_feature=self.feature_dim, hidden_size=self.args.hidden_discriminator).cuda()
+        self.domain_adv = DomainAdversarialLoss(self.domain_discriminator).cuda()
+        self.eps = 1e-6
+
+    def memorize(self, timestamp, mean_logStd_t=None):
+        with torch.no_grad():
+            t = timestamp - self.args.init_timestamp
+            if t <= 1:
+                self.memory_pool[t] = Variable(self.init_pool[t].detach().clone(), requires_grad=False).cuda()
+            else:
+                self.memory_pool[t] = Variable(mean_logStd_t.detach().clone(), requires_grad=False).cuda()
+
+    def get_previous_mean_logStd(self, timestamp):
+        t = timestamp - self.args.init_timestamp
+        if t < 2:
+            raise RuntimeError(f' Timestamp {timestamp} does not require previous statistics!')
+        else:
+            previous_mean_logStd = None
+            lower_bound = 0
+            if self.args.memory_pool is not None:
+                lower_bound = t - self.args.memory_pool if t - self.args.memory_pool > 0 else 0
+
+            for i in range(lower_bound, t):
+                mean_logStd_i = self.memory_pool[i]
+                if previous_mean_logStd is None:
+                    previous_mean_logStd = mean_logStd_i
+                else:
+                    previous_mean_logStd = torch.cat((previous_mean_logStd, mean_logStd_i), dim=0)
+            return previous_mean_logStd.cuda()
+
+    def foward_for_FeatureDistritbuion(self, previous_mean_logStd):
+        future_out = None
+        num_previous_domain = previous_mean_logStd.size(0)
+        s = 0
+        loss_consistency = None
+        for k in range(1, self.args.scale + 1):
+            if k == 1:
+                out_k = self.attention_dict[k](previous_mean_logStd) # out_k.shape: [1, t-1, 2*feature_dim]
+                future_out = torch.mean(out_k, dim=1)
+                s += 1
+            else:
+                if (num_previous_domain < k + 1) or (self.args.scale > self.args.split_time - self.args.init_timestamp):
+                    pass
+                else:   # concatenate version
+                    rearranged_input = previous_mean_logStd.view(1, previous_mean_logStd.shape[0], 1, previous_mean_logStd.shape[1])
+                    patches = self.conv1d_dict[k](rearrange(rearranged_input, 'b w h c -> b c h w'))
+                    patches = torch.reshape(patches, (1, previous_mean_logStd.shape[1], k*1, patches.shape[-1]))  # patches.shape:[1, d, k, slices]
+                    patches = patches.permute(0, 3, 2, 1)    # to shape:[1, slices, k, d]
+                    patches = rearrange(patches, 'b l k d -> b l (k d)')
+                    concate_patches = patches.squeeze(0)   # to [l, kd]
+
+                    out_k = self.attention_dict[k](concate_patches)       # out_k.shape: [1, l, kd]
+                    last_out_k = torch.mean(out_k, dim=1)
+                    last_out_k = last_out_k.view(1, k, 2*self.feature_dim)
+
+                    future_out = future_out + last_out_k[:, -1, :]
+
+                    temp_loss_consistency = torch.norm(previous_mean_logStd[-(k - 1):, :] - last_out_k.squeeze(0)[:k-1, :], p=2, dim=1)
+                    if loss_consistency is None:
+                        loss_consistency = torch.mean(temp_loss_consistency)
+                    else:
+                        loss_consistency += torch.mean(temp_loss_consistency)
+                    s += 1
+        future_out = future_out / s
+        return future_out, loss_consistency
+
+    def foward_encoder(self, x):
+        f = self.enc(x)
+        return f
+
+    def foward_classifier(self, normalized_f):
+        logits = self.classifier(normalized_f)
+        return logits
+
+    def forward_evaluate(self, x, mean, std):
+        f = self.enc(x)
+        f = (f - mean) / (std + self.eps)
+        logits = self.classifier(f)
+        return logits
+
+    def forward_domain_discriminator(self, f, previous_mean_logStd):
+        b, d = f.size()
+        l = previous_mean_logStd.size(0)
+
+        for i in range(0, l):
+            previous_mean_i = previous_mean_logStd[i, :self.feature_dim]
+            previous_logStd_i = previous_mean_logStd[i, self.feature_dim:]
+            std_i = torch.exp(previous_logStd_i)
+
+            if self.args.truncate is not None:
+                w = torch.empty(b, 1).cuda()
+                nn.init.trunc_normal_(w, mean=0.0, std=1.0, a=-self.args.truncate, b=self.args.truncate)
+            else:
+                distri = torch.distributions.Normal(loc=torch.tensor([0.0]), scale=torch.tensor([1.0]))
+                w = distri.sample((b,)).cuda()
+
+            temp_features_i = previous_mean_i.view(1, d).expand(b, d) + w.expand(b, d) * std_i.view(1, d).expand(b, d)
+
+            if i == 0:
+                previous_f = temp_features_i.detach()
+            else:
+                previous_f = torch.cat((previous_f, temp_features_i.detach()), dim=0)
+        adv_loss = self.domain_adv(previous_f, f)
+        return adv_loss
+
+    def rest_discriminator_lr(self, max_iters):
+        self.domain_adv.reset(max_iters=max_iters)
+
+    def get_parameters(self, lr):
+        params_list = []
+        for k in range(1, self.args.scale + 1):
+            params_list.append({"params": self.attention_dict[k].parameters(), 'lr': 1 * lr})
+        params_list.extend([
+                {"params": self.enc.parameters(), 'lr': 1 * lr},
+                {"params": self.classifier.parameters(), 'lr': 1 * lr},
+                {"params": self.init_pool[0], 'lr': 1 * lr},
+                {"params": self.init_pool[1], 'lr': 1 * lr},
+                {"params": self.domain_discriminator.parameters(), 'lr': 1 * lr},
+            ]
+        )
+        return params_list
+
---
+++ networks/submodules.py
@@ -0,0 +1,231 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable, Function
+from einops import rearrange
+from typing import List, Dict, Optional, Any, Tuple
+from .init_func import init_weights
+
+
+
+
+def binary_accuracy(output: torch.Tensor, target: torch.Tensor):
+    """Computes the accuracy for binary classification"""
+    with torch.no_grad():
+        batch_size = target.size(0)
+        pred = (output >= 0.5).float().t().view(-1)
+        correct = pred.eq(target.view(-1)).float().sum()
+        correct.mul_(100. / batch_size)
+        return correct
+
+
+
+class GradientReverseFunction(Function):
+    @staticmethod
+    def forward(ctx: Any, input: torch.Tensor, coeff: Optional[float] = 1.) -> torch.Tensor:
+        ctx.coeff = coeff
+        output = input * 1.0
+        return output
+
+    @staticmethod
+    def backward(ctx: Any, grad_output: torch.Tensor) -> Tuple[torch.Tensor, Any]:
+        return grad_output.neg() * ctx.coeff, None
+
+
+class GradientReverseLayer(nn.Module):
+    def __init__(self):
+        super(GradientReverseLayer, self).__init__()
+
+    def forward(self, *input):
+        return GradientReverseFunction.apply(*input)
+
+
+
+class WarmStartGradientReverseLayer(nn.Module):
+    """Gradient Reverse Layer :math:`\mathcal{R}(x)` with warm start
+
+        The forward and backward behaviours are:
+
+        .. math::
+            \mathcal{R}(x) = x,
+
+            \dfrac{ d\mathcal{R}} {dx} = - \lambda I.
+
+        :math:`\lambda` is initiated at :math:`lo` and is gradually changed to :math:`hi` using the following schedule:
+
+        .. math::
+            \lambda = \dfrac{2(hi-lo)}{1+\exp(- α \dfrac{i}{N})} - (hi-lo) + lo
+
+        where :math:`i` is the iteration step.
+
+        Parameters:
+            - **alpha** (float, optional): :math:`α`. Default: 1.0
+            - **lo** (float, optional): Initial value of :math:`\lambda`. Default: 0.0
+            - **hi** (float, optional): Final value of :math:`\lambda`. Default: 1.0
+            - **max_iters** (int, optional): :math:`N`. Default: 1000
+            - **auto_step** (bool, optional): If True, increase :math:`i` each time `forward` is called.
+              Otherwise use function `step` to increase :math:`i`. Default: False
+        """
+
+    def __init__(self, alpha: Optional[float] = 1.0, lo: Optional[float] = 0.0, hi: Optional[float] = 1.,
+                 max_iters: Optional[int] = 1000., auto_step: Optional[bool] = False):
+        super(WarmStartGradientReverseLayer, self).__init__()
+        self.alpha = alpha
+        self.lo = lo
+        self.hi = hi
+        self.iter_num = 0
+        self.max_iters = max_iters
+        self.auto_step = auto_step
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        """"""
+        coeff = np.float(
+            2.0 * (self.hi - self.lo) / (1.0 + np.exp(- self.alpha * self.iter_num / self.max_iters))
+            - (self.hi - self.lo) + self.lo
+        )
+        if self.auto_step:
+            self.step()
+        return GradientReverseFunction.apply(input, coeff)
+
+    def step(self):
+        """Increase iteration number :math:`i` by 1"""
+        self.iter_num += 1
+
+    def reset(self, max_iters=None):
+        self.iter_num = 0
+        if max_iters is not None:
+            self.max_iters = max_iters
+
+
+
+
+class DomainAdversarialLoss(nn.Module):
+    r"""The `Domain Adversarial Loss <https://arxiv.org/abs/1505.07818>`_
+
+    Domain adversarial loss measures the domain discrepancy through training a domain discriminator.
+    Given domain discriminator :math:`D`, feature representation :math:`f`, the definition of DANN loss is
+
+    .. math::
+        loss(\mathcal{D}_s, \mathcal{D}_t) &= \mathbb{E}_{x_i^s \sim \mathcal{D}_s} log[D(f_i^s)] \\
+        &+ \mathbb{E}_{x_j^t \sim \mathcal{D}_t} log[1-D(f_j^t)].\\
+
+    Parameters:
+        - **domain_discriminator** (class:`nn.Module` object): A domain discriminator object, which predicts
+          the domains of features. Its input shape is (N, F) and output shape is (N, 1)
+        - **reduction** (string, optional): Specifies the reduction to apply to the output:
+          ``'none'`` | ``'mean'`` | ``'sum'``. ``'none'``: no reduction will be applied,
+          ``'mean'``: the sum of the output will be divided by the number of
+          elements in the output, ``'sum'``: the output will be summed. Default: ``'mean'``
+
+    Inputs: f_s, f_t
+        - **f_s** (tensor): feature representations on source domain, :math:`f^s`
+        - **f_t** (tensor): feature representations on target domain, :math:`f^t`
+
+    Shape:
+        - f_s, f_t: :math:`(N, F)` where F means the dimension of input features.
+        - Outputs: scalar by default. If :attr:``reduction`` is ``'none'``, then :math:`(N, )`.
+
+    Examples::
+        >>> discriminator = DomainDiscriminator(in_feature=1024, hidden_size=1024)
+        >>> loss = DomainAdversarialLoss(discriminator, reduction='mean')
+        >>> # features from source domain and target domain
+        >>> f_s, f_t = torch.randn(20, 1024), torch.randn(20, 1024)
+        >>> output = loss(f_s, f_t)
+    """
+
+    def __init__(self, domain_discriminator: nn.Module, reduction: Optional[str] = 'mean', max_iters=1000):
+        super(DomainAdversarialLoss, self).__init__()
+        self.grl = WarmStartGradientReverseLayer(alpha=1., lo=0., hi=1., max_iters=max_iters, auto_step=True)
+        self.domain_discriminator = domain_discriminator
+        self.bce = nn.BCELoss(reduction=reduction)
+        self.domain_discriminator_accuracy = None
+
+    def forward(self, f_s: torch.Tensor, f_t: torch.Tensor) -> torch.Tensor:
+        f = self.grl(torch.cat((f_s, f_t), dim=0))
+        d = self.domain_discriminator(f)
+        d_s = d[:f_s.size(0), :]
+        d_t = d[f_s.size(0):, :]
+        d_label_s = torch.ones((f_s.size(0), 1)).to(f_s.device)
+        d_label_t = torch.zeros((f_t.size(0), 1)).to(f_t.device)
+        self.domain_discriminator_accuracy = 0.5 * (binary_accuracy(d_s, d_label_s) + binary_accuracy(d_t, d_label_t))
+        return 0.5 * (self.bce(d_s, d_label_s) + self.bce(d_t, d_label_t)), self.domain_discriminator_accuracy
+
+    def reset(self, max_iters=None):
+        self.grl.reset(max_iters)
+
+
+
+class DomainDiscriminator(nn.Module):
+    r"""Domain discriminator model from
+    `"Domain-Adversarial Training of Neural Networks" <https://arxiv.org/abs/1505.07818>`_
+
+    Distinguish whether the input features come from the source domain or the target domain.
+    The source domain label is 1 and the target domain label is 0.
+
+    Parameters:
+        - **in_feature** (int): dimension of the input feature
+        - **hidden_size** (int): dimension of the hidden features
+
+    Shape:
+        - Inputs: (minibatch, `in_feature`)
+        - Outputs: :math:`(minibatch, 1)`
+    """
+
+    def __init__(self, in_feature: int, hidden_size: int):
+        super(DomainDiscriminator, self).__init__()
+        self.layer1 = nn.Linear(in_feature, hidden_size)
+        self.bn1 = nn.BatchNorm1d(hidden_size)
+        self.relu1 = nn.ReLU()
+        self.layer2 = nn.Linear(hidden_size, hidden_size)
+        self.bn2 = nn.BatchNorm1d(hidden_size)
+        self.relu2 = nn.ReLU()
+        self.layer3 = nn.Linear(hidden_size, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """"""
+        x = self.relu1(self.bn1(self.layer1(x)))
+        x = self.relu2(self.bn2(self.layer2(x)))
+        y = self.sigmoid(self.layer3(x))
+        return y
+
+
+
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads=8, dim_head=64, dropout=0.):
+        super().__init__()
+        inner_dim = dim_head * heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.attend = nn.Softmax(dim=-1)
+        self.dropout = nn.Dropout(dropout)
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias=False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        '''
+        x.shape = [t-1, 2 * feature_dim]
+        '''
+        x = x.view(1, x.shape[0], x.shape[1])   # x.shape: [1, t-1, 2*feature_dim]
+        qkv = self.to_qkv(x).chunk(3, dim=-1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.heads), qkv)
+
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        attn = self.attend(dots)
+        attn = self.dropout(attn)
+
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
